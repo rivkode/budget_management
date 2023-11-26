1566b1186c7b1a6d6119fcb543f9ed9c6bedec89
jonghun <jonghuncu@gmail.com>
2023-11-26 18:59:07 +0900

    feat : SecurityConfig
    아래 사항 포함
    
    - JwtUtil
    - UserDetailsServiceImpl
    - AuthenticationConfiguration

--- src/main/java/com/example/budget_management/system/security/SecurityConfig.java
+++ src/main/java/com/example/budget_management/system/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,12 +19,34 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    // Authenticatoin, 토큰에 대해 인증
+//    @Bean
+//    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+//
+//    }
+
+
+    // Authorization, 식별된 사용자에 대해 권한 부여, 인가
+//    @Bean
+//    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+//
+//    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-
-
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        // Session 사용 X
         httpSecurity.sessionManagement(sessionManagement ->
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.authorizeHttpRequests(authorizeHttpRequests ->
