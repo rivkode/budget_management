b2ed01778ef3ce9e2ac67632bbb55e10e885e87e
jonghun <jonghuncu@gmail.com>
2023-11-26 21:44:56 +0900

    feat : category 엔티티

--- src/main/java/com/example/budget_management/domain/category/Category.java
+++ src/main/java/com/example/budget_management/domain/category/Category.java
@@ -1,5 +1,19 @@
 package com.example.budget_management.domain.category;
 
-public class Category {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor
+@Getter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
