154af2f025fd4e8b4c79a54bc303264b7ad680dc
jonghun <jonghuncu@gmail.com>
2023-11-26 19:40:45 +0900

    fix : email 추가

--- src/main/java/com/example/budget_management/domain/user/User.java
+++ src/main/java/com/example/budget_management/domain/user/User.java
@@ -22,7 +22,8 @@ public class User {
     private String password;
 
     @Builder
-    public User(String username, String password) {
+    public User(String email, String username, String password) {
+        this.email = email;
         this.username = username;
         this.password = password;
     }
