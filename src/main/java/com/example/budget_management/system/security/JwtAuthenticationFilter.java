a3424929a89cfd83b7d62bede24df650214807c6
jonghun <jonghuncu@gmail.com>
2023-11-26 18:56:58 +0900

    feat : 인증, 인가 빈 클래스

---
+++ src/main/java/com/example/budget_management/system/security/JwtAuthenticationFilter.java
@@ -0,0 +1,11 @@
+package com.example.budget_management.system.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j(topic = "로그인, JWT 생성")
+public class JwtAuthenticationFilter {
+
+//    private final JwtUtil jwtUtil;
+
+}
---
+++ src/main/java/com/example/budget_management/system/security/JwtAuthorizationFilter.java
@@ -0,0 +1,8 @@
+package com.example.budget_management.system.security;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "JWT 검증, 인가")
+public class JwtAuthorizationFilter {
+
+}
