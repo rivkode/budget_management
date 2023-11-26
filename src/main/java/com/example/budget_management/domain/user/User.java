77250dfa9a284d7a118880f1dd9eafe5e1ea4acb
jonghun <jonghuncu@gmail.com>
2023-11-26 18:58:11 +0900

    docs : rename User

--- src/main/java/com/example/budget_management/domain/user/User.java
+++ src/main/java/com/example/budget_management/domain/user/User.java
@@ -11,17 +11,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Getter
-public class Users {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String email;
     private String username;
     private String password;
 
     @Builder
-    public Users(String username, String password) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
