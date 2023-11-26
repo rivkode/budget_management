3074c3f52a056c00ddb25bf9712996f2302067f2
jonghun <jonghuncu@gmail.com>
2023-11-26 18:57:47 +0900

    feat : UserDetailsService 생성

---
+++ src/main/java/com/example/budget_management/system/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.budget_management.system.security;
+
+import com.example.budget_management.domain.user.User;
+import com.example.budget_management.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found" + email));
+        return new UserDetailsImpl(user);
+    }
+}
