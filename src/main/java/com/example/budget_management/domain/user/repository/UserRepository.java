a6d39ff36c0e1550ade57003071dd0817366a060
jonghun <jonghuncu@gmail.com>
2023-11-26 18:57:58 +0900

    feat : UserRepository 생성

---
+++ src/main/java/com/example/budget_management/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.budget_management.domain.user.repository;
+
+import com.example.budget_management.domain.user.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
