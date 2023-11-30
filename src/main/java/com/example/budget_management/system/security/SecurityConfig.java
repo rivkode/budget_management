21df51943d4e0f4e9e26f7fa11c060e26a088c4f
jonghun <jonghuncu@gmail.com>
2023-12-01 07:26:45 +0900

    fix : api permit 제거

--- src/main/java/com/example/budget_management/system/security/SecurityConfig.java
+++ src/main/java/com/example/budget_management/system/security/SecurityConfig.java
@@ -55,7 +55,6 @@ public class SecurityConfig {
         httpSecurity.authorizeHttpRequests(authorizeHttpRequests ->
                 authorizeHttpRequests
                         .requestMatchers(HttpMethod.POST, "/api/users/**").permitAll() // '/api/users'로 시작하는 요청 중 모든 POST 접근 허가
-                        .requestMatchers("/api/budgets/**", "/api/categories/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/**").permitAll() // swagger-ui 와 관련된 모든 요청 접근 허가
                         .requestMatchers(HttpMethod.POST, "/api/verifications").permitAll() // 가입승인 API 요청 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
