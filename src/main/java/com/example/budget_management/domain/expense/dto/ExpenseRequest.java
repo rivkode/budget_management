4decaf9f138e124c70f99d81c6c0e0150855cc8a
jonghun <jonghuncu@gmail.com>
2023-12-01 07:15:38 +0900

    fix : month 추가

--- src/main/java/com/example/budget_management/domain/expense/dto/ExpenseRequest.java
+++ src/main/java/com/example/budget_management/domain/expense/dto/ExpenseRequest.java
@@ -4,6 +4,8 @@ import com.example.budget_management.domain.budget.Budget;
 import com.example.budget_management.domain.budget.Category;
 import com.example.budget_management.domain.expense.Expense;
 import com.example.budget_management.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -14,6 +16,8 @@ public class ExpenseRequest {
     private Long budgetId;
     private Long expenseId;
     private String categoryName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy hh:mm:ss.SSS", timezone = "Asia/Seoul")
+    private LocalDateTime endAt;
 
     public Expense toEntity(User user, Budget budget, Category category) {
         return Expense.builder()
@@ -21,6 +25,7 @@ public class ExpenseRequest {
                 .category(category)
                 .budget(budget)
                 .user(user)
+                .month(endAt)
                 .build();
     }
 
