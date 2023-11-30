939c4c469f4cfd33ffdcf3d040c8ecdd826bf215
jonghun <jonghuncu@gmail.com>
2023-12-01 00:05:55 +0900

    feat : budget 엔티티

--- src/main/java/com/example/budget_management/domain/budget/Budget.java
+++ src/main/java/com/example/budget_management/domain/budget/Budget.java
@@ -1,5 +1,53 @@
 package com.example.budget_management.domain.budget;
 
+import com.example.budget_management.domain.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class Budget {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long amount;
+
+    private LocalDateTime month;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+
+    @Builder
+    public Budget(Long amount, User user, Category category, LocalDateTime month) {
+        this.amount = amount;
+        this.user = user;
+        this.category = category;
+        this.month = month;
+    }
+
+    public Budget update(Long amount, Category category) {
+        this.amount = amount;
+        this.category = category;
+
+        return this;
+    }
 }
