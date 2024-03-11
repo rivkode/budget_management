0188ed88551cfeee6db865e1f0f5d41a1de29b76
rivkode <jonghuncu@gmail.com>
2024-03-11 18:01:51 +0900

    어노테이션 삭제

--- src/main/java/com/example/budget_management/domain/budget/service/BudgetService.java
+++ src/main/java/com/example/budget_management/domain/budget/service/BudgetService.java
@@ -12,9 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
+
 @RequiredArgsConstructor
-@Transactional
+@Service
 public class BudgetService {
 
     private final BudgetRepository budgetRepository;
