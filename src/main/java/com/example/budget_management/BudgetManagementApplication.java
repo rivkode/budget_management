80d0068f25198ebd89cba04370cbe2e0ae92c42e
rivkode <jonghuncu@gmail.com>
2024-03-21 15:26:47 +0900

    auditing, enableCaching 어노테이션

--- src/main/java/com/example/budget_management/BudgetManagementApplication.java
+++ src/main/java/com/example/budget_management/BudgetManagementApplication.java
@@ -2,8 +2,12 @@ package com.example.budget_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //auditing 기능 위한 추가
 @SpringBootApplication
+@EnableCaching // 레디스 캐싱 하기위해 추가, @Cacheable 같은 어노테이션을 인식 하게 함
 public class BudgetManagementApplication {
 
     public static void main(String[] args) {
