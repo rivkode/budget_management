ef0be28637e7f5d37a1f5c0dfd808724febb661d
rivkode <jonghuncu@gmail.com>
2024-03-21 15:27:49 +0900

    cors 설정

--- src/main/java/com/example/budget_management/system/security/SecurityConfig.java
+++ src/main/java/com/example/budget_management/system/security/SecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -69,11 +74,13 @@ public class SecurityConfig {
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.authorizeHttpRequests(authorizeHttpRequests ->
                 authorizeHttpRequests
-                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/users/**")).permitAll()  // '/api/users'로 시작하는 요청 중 모든 POST 접근 허가
-                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/verifications")).permitAll()// 가입승인 API 요청 허가
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/swagger-ui/**")).permitAll()  // swagger-ui 와 관련된 모든 요청 접근 허가
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/v3/**")).permitAll()
+//                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/users/**")).permitAll()  // '/api/users'로 시작하는 요청 중 모든 POST 접근 허가
+//                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/verifications")).permitAll()// 가입승인 API 요청 허가
+//                        .requestMatchers(AntPathRequestMatcher.antMatcher("/swagger-ui/**")).permitAll()  // swagger-ui 와 관련된 모든 요청 접근 허가
+//                        .requestMatchers(AntPathRequestMatcher.antMatcher("/v3/**")).permitAll()
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll() // h2 console 허용
+//                        .requestMatchers("/user", "/user/logout", "/user/password", "/user/password/email-link", "/user/email-notification").authenticated()
+                        .requestMatchers("/user/**").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
@@ -84,4 +91,19 @@ public class SecurityConfig {
         return httpSecurity.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowCredentials(true);
+
+        // URL 패턴에 따라 다른 CORS 구성을 적용할 수 있게 해줌
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 API Endpoint 에 동일한 configuration 적용
+
+        return source;
+    }
 }
