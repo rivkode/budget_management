dd368aa42cf5851bb44f1f7c48414aa2664ed079
rivkode <jonghuncu@gmail.com>
2024-03-21 15:25:13 +0900

    redis Repository Config

---
+++ src/main/java/com/example/budget_management/system/common/cache/redis/config/RedisRepositoryConfig.java
@@ -0,0 +1,21 @@
+package com.example.budget_management.system.common.cache.redis.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableRedisRepositories(basePackages = "com.example.budget_management.system.common.cache.redis.repository")
+public class RedisRepositoryConfig {
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+}
